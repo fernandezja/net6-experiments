9abcac1025e4255d39695e1c8137aee3c86cbb61
fernandezja <fernandezja@gmail.com>
2021-08-16 11:24:12 -0300

    [003-websocket-server] Echo/Ping with websocket server

---
+++ 003-websocket-server/Controllers/WebsocketController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace _003_websocket_server.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class WebsocketController : ControllerBase
+{   
+    private readonly ILogger<WebsocketController> _logger;
+
+    public WebsocketController(ILogger<WebsocketController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("/echo")]
+    public async Task EchoEvent()
+    {
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            _logger.Log(LogLevel.Information, "WebSocket connection established");
+            await EchoEventAction(webSocket);
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = 400;
+        }
+    }
+
+
+    private async Task EchoEventAction(WebSocket webSocket)
+    {
+        var buffer = new byte[1024 * 4];
+        var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        _logger.Log(LogLevel.Information, "Message received from Client");
+
+        while (!result.CloseStatus.HasValue)
+        {
+            var message = Encoding.UTF8.GetBytes($"Ping {DateTime.Now.ToString("ddd yyyy-MM-dd HH:mm:ss.FFF")}");
+
+            await webSocket.SendAsync(new ArraySegment<byte>(message, 0, message.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
+            _logger.Log(LogLevel.Information, "Message sent to Client");
+
+            result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            _logger.Log(LogLevel.Information, "Message received from Client");
+
+        }
+
+        await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        _logger.Log(LogLevel.Information, "WebSocket connection closed");
+    }
+
+}
---
+++ 003-websocket-server/Program.cs
@@ -0,0 +1,24 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseWebSockets();
+
+app.MapControllers();
+
+app.Run();
