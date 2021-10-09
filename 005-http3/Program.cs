b71943ee04a2c3d7195c376f8ebe4f4c814fe0ae
fernandezja <fernandezja@gmail.com>
2021-10-08 21:57:48 -0300

    Example 005 htt3p

---
+++ 005-http3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    options.Listen(IPAddress.Any, 5001, listenOptions =>
+    {
+        // Use HTTP/3
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+        listenOptions.UseHttps();
+    });
+});
