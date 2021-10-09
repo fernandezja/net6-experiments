562253cb8dfbdf762c5c02e9a81392b277c9faa5
fernandezja <fernandezja@gmail.com>
2021-10-08 21:59:05 -0300

    Example 007 graphics

---
+++ 007-graphics/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+using System.Drawing.Imaging;
+
+Console.WriteLine("Create PNG!");
+
+var bmp = new Bitmap(50, 50);
+
+using (Graphics graph = Graphics.FromImage(bmp))
+{
+    Rectangle imageSize = new Rectangle(0, 0, 50, 50);
+    graph.FillRectangle(Brushes.White, imageSize);
+}
+
+bmp.Save("demo.png", ImageFormat.Png);
