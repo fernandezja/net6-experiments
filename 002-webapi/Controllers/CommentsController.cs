de0f1bc12808059b1d7835cf2e1c0ec3a3ab7723
Jose A. Fernandez <fernandezja@gmail.com>
2022-10-14 18:16:24 -0300

    [002-WebApi] CommentsController

---
+++ 002-webapi/Controllers/CommentsController.cs
@@ -0,0 +1,59 @@
+﻿using _002_webapi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _002_webapi.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class CommentsController : ControllerBase
+{
+    
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public CommentsController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var result = new ContentResult()
+        {
+            Content = "Comments OK",
+            ContentType = "text/plain"
+        };
+
+        return result;
+    }
+
+    [HttpPut]
+    [Route("/comments/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CommentDTO))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)] 
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Update(int id, CommentDTO commentDTO)
+    {
+        if (commentDTO == null)
+            return BadRequest();
+
+
+        if (commentDTO.UserId <= 0) {
+            //return Forbid();
+            return StatusCode(StatusCodes.Status403Forbidden);
+        }
+           
+
+        if (ModelState.IsValid)
+        {
+            return new OkObjectResult(commentDTO);
+        }
+
+        return NotFound();
+    }
+}
---
+++ 002-webapi/Entities/CommentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace _002_webapi.Entities
+{
+    public class CommentDTO
+    {
+        public int CommentId { get; set; }
+        public string Message { get; set; }
+        public int UserId { get; set; }
+    }
+}
