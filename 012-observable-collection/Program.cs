4a1efca96eeb6471ff373b8b21cd526b606b8d93
Jose A. Fernandez <fernandezja@gmail.com>
2022-10-22 08:21:12 -0300

    [012] Observable Collection

---
+++ 012-observable-collection/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.ObjectModel;
+using static System.Linq.Enumerable;
+
+Console.WriteLine("ObservableCollection!");
+
+var oc = new ObservableCollection<string>
+{
+    "a",
+    "b"
+};
+
+for (int index = 1; index <= 5; index++)
+{
+    oc.Add(index.ToString());
+}
+
+void Printer() {
+    foreach (var item in oc)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+oc.CollectionChanged += Oc_CollectionChanged;
+
+void Oc_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+{
+    Console.WriteLine("Oc_CollectionChanged...");
+    Printer();
+}
+
+while (true)
+{
+    Console.Write("Add new item:");
+    var newItem = Console.ReadLine();
+    if (!string.IsNullOrEmpty(newItem))
+    {
+        oc.Add(newItem);
+    }
+}
+
